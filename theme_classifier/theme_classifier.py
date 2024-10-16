04e056f1e1eb8dc1516f957fdf54c87041de0e6a
Koutilya Pande <koutilyapande26@gmail.com>
2024-10-16 13:48:53 -0400

    edit theme_classifier.py

--- theme_classifier/theme_classifier.py
+++ theme_classifier/theme_classifier.py
@@ -42,7 +42,7 @@ class ThemeClassifier():
         
         # Run Model
         theme_output = self.theme_classifier(
-            script_batches[:2],
+            script_batches,
             self.theme_list,
             multi_label=True
         )
@@ -67,7 +67,7 @@ class ThemeClassifier():
 
         # load Dataset
         df = load_subtitles_dataset(dataset_path)
-        df = df.head(2)
+        
         print(f"Loaded dataset shape: {df.shape}")
         
 
